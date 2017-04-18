d3d728c938362ecb5f0c5ee508ef4fb9e8859b48
Mikfor <ibsenb@ibsens-mbp.eduroam.wireless.dtu.dk>
2017-04-18 12:59:38 +0200

    Små rettelser

--- GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/ChatFragment.java
+++ GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/ChatFragment.java
@@ -1,32 +1,31 @@
 package com.u_09.galgeleg.View;
 
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.u_09.galgeleg.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by ibsenb on 18/04/2017.
  */
 
-public class Admin_Fragment extends Fragment implements View.OnClickListener {
+public class ChatFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.highscore_fragment, container, false);
-
-        return view;
-
+        return;
     }
 
     @Override
-    public void onClick(View v) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        
     }
-
 }
