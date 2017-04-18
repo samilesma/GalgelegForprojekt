92c7230c2681479fed5b8470aeb252b24d72a64f
Mikfor <ibsenb@ibsens-mbp.eduroam.wireless.dtu.dk>
2017-04-18 12:52:24 +0200

    Laver admin panel på Android
    Er igang med at lave admin panelet til android

--- GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/Admin_Fragment.java
+++ GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/Admin_Fragment.java
@@ -1,8 +1,32 @@
 package com.u_09.galgeleg.View;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.u_09.galgeleg.R;
+
 /**
  * Created by ibsenb on 18/04/2017.
  */
 
-public class Admin_Fragment {
+public class Admin_Fragment extends Fragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.highscore_fragment, container, false);
+
+        return view;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        
+    }
+
 }
