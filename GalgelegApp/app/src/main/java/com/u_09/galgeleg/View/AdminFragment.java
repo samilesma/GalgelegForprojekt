8a044aa24d0cb9729d604524aaac064f41d5a553
Mikfor <ibsenb@ibsens-mbp.eduroam.wireless.dtu.dk>
2017-04-18 13:53:00 +0200

    Admin Fragmentet
    Er igang med design af Admin fragmentet

--- GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/AdminFragment.java
+++ GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/AdminFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.u_09.galgeleg.R;
 
@@ -14,11 +16,15 @@ import com.u_09.galgeleg.R;
 
 public class AdminFragment extends Fragment implements View.OnClickListener {
 
+    private Spinner dropDown;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.chat_fragment, container, false);
-
+        View view = inflater.inflate(R.layout.admin_fragment, container, false);
+        Spinner dropdown = (Spinner) view.findViewById(R.id.dropDown);
+        ArrayAdapter<String> users = new ArrayAdapter<String>(AdminFragment.this, android.R.layout.simple_spinner_dropdown_item,items);
         return view;
 
     }
