43dc35b01479a6df7dc4d0baab84debe1c2377dc
Mikfor <ibsenb@ibsens-mbp.eduroam.wireless.dtu.dk>
2017-04-18 12:49:35 +0200

    Chat funktioner færdige

--- GalgelegApp/app/src/main/java/com/u_09/galgeleg/Model/Function.java
+++ GalgelegApp/app/src/main/java/com/u_09/galgeleg/Model/Function.java
@@ -1,6 +1,7 @@
 package com.u_09.galgeleg.Model;
 
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -17,12 +18,28 @@ public class Function {
         con.update("INSERT INTO messages (sid,msg,timestamp) VALUES ('"+sid+"',?,'"+timestamp+"')",new String[]{"l",Long.toString(timestamp)});
     }
 
-    public ArrayList<ArrayList<String>> getAllMessages(int id){
+    public ArrayList<ArrayList<String>> getAllMessages() throws SQLException{
         ArrayList<ArrayList<String>> msg = new ArrayList<ArrayList<String>>();
         msg.add(new ArrayList<String>());
         msg.add(new ArrayList<String>());
+        msg.add(new ArrayList<String>());
+        ResultSet rs = con.select("SELECT sid,msg,timestamp FROM chat WHERE deleted=0");
+        while(rs.next()){
+            msg.get(0).add(rs.getString("sid"));
+            msg.get(1).add(rs.getString("msg"));
+            msg.get(2).add(rs.getString("timestamp"));
+        }
+
+        return msg;
 
     }
+
+    public void deleteMessage(int id) throws SQLException{
+        con.update("UPDATE chat SET deleted=1 WHERE id="+id);
+    }
+
+
+
 
 
 }
---
+++ GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/Admin_Fragment.java
@@ -0,0 +1,8 @@
+package com.u_09.galgeleg.View;
+
+/**
+ * Created by ibsenb on 18/04/2017.
+ */
+
+public class Admin_Fragment {
+}
