ef52fce8efd97611a3e7aea74f80c1a42c001994
Mikfor <ibsenb@ibsens-mbp.eduroam.wireless.dtu.dk>
2017-04-18 12:18:26 +0200

    Rettet lille fejl

--- GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/ChatFragment.java
+++ GalgelegApp/app/src/main/java/com/u_09/galgeleg/View/ChatFragment.java
@@ -1,12 +1,14 @@
 package com.u_09.galgeleg.View;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+
+import com.u_09.galgeleg.R;
 
 import java.util.ArrayList;
 
@@ -16,10 +18,10 @@ import java.util.ArrayList;
 
 public class ChatFragment extends Fragment implements AdapterView.OnItemClickListener {
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        return;
     }
 
     @Override
