bdf6f265f20776c6841725b5fe9ddf7388afdfc1
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-04-27 17:24:36 +0200

    Please ik merge 2.0

--- Galgeleg/src/galgeleg/GalgelegServer.java
+++ Galgeleg/src/galgeleg/GalgelegServer.java
@@ -1,18 +1,25 @@
-
 package galgeleg;
 
+import java.util.Map;
+import java.util.Scanner;
 import javax.xml.ws.Endpoint;
 
 public class GalgelegServer {
     public static void main(String[] arg) throws Exception
-	{
-		
-                System.out.println("publicerer Galgeleg");
-		GalgeI g = new Galgelogik();
-                
-                Endpoint.publish("http://[::]:55556/galgeleg", g);
- //               Endpoint.publish("http://ubuntu4.javabog.dk:55556/galgeleg", g);
-
-                System.out.println("Galgeleg publiceret.");
-	}
+    {
+        System.out.println("publicerer Galgeleg");
+        GalgeI g = new Galgelogik();
+        Endpoint.publish("http://[::]:55556/galgeleg", g);
+//        Endpoint.publish("http://ubuntu4.javabog.dk:55556/galgeleg", g);
+        
+        System.out.println("Galgeleg publiceret.");
+        
+        while(true)
+        {
+            Scanner reader = new Scanner(System.in);
+            String input = reader.nextLine();
+            
+            if(input.equals("print")) g.print();
+        }
+    }
 }
